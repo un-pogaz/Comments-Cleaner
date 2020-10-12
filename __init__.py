461f77917a2d80ec430dbd4335b151cfc3c81ec1
un-pogaz <46523284+un-pogaz@users.noreply.github.com>
2020-10-12 13:00:01 +0200

    v1.1

--- __init__.py
+++ __init__.py
@@ -24,7 +24,7 @@ class ActionCommentCleaner(InterfaceActionBase):
     description             = _('Remove the scraps CSS in HTML comments')
     supported_platforms     = ['windows', 'osx', 'linux']
     author                  = 'un_pogaz'
-    version                 = (1, 0, 1)
+    version                 = (1, 1, 0)
     minimum_calibre_version = (2, 0, 0)
 
     #: This field defines the GUI plugin class that contains all the code
